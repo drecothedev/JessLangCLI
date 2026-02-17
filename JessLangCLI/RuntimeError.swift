e3d4e9ef35fd7dd33c5fae94f3018143c47c6d2f
drecothedev <drecothedev@gmail.com>
2026-02-16 22:26:42 -0800

    Updated Interpreter. The program can now produce arithmetic statements. i will be adding state and statement functionality next

---
+++ JessLangCLI/RuntimeError.swift
@@ -0,0 +1,8 @@
+//
+//  RuntimeError.swift
+//  JessLangCLI
+//
+//  Created by Andre jones on 2/15/26.
+//
+
+import Foundation
---
+++ JessLangCLI/Statement.swift
@@ -0,0 +1,8 @@
+//
+//  Statement.swift
+//  JessLangCLI
+//
+//  Created by Andre jones on 2/16/26.
+//
+
+import Foundation
